96e6ea62a0c29cf483d5525febeab1ddc5ff2ba1
jvansulli <paanexweb@gmail.com>
2026-07-30 18:09:34 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    document.getElementById("message").innerHTML =
+    "I hope this year brings you everything you wish for ❤️<br><br>" +
+    "You are a really special person, and I'm happy I got to know you.";
+}
